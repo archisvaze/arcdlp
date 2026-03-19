f90e84ddf2ecb4bba8bc36b1141bbc679026e696
Archis <vazearchis@gmail.com>
2026-03-19 14:51:03 +0530

    formatting changes

--- src/main/utils.js
+++ src/main/utils.js
@@ -1,11 +1,13 @@
-const DEV_MODE = !!(process.env.DEV_MODE || process.argv.includes('--dev') || process.argv.includes('--dev-mode'));
+const DEV_MODE =
+    !!(process.env.DEV_MODE || process.argv.includes("--dev") ||
+        process.argv.includes("--dev-mode"));
 
 function log(...args) {
-    if (DEV_MODE) console.log('[App]', ...args);
+    if (DEV_MODE) console.log("[App]", ...args);
 }
 
 function logError(...args) {
-    console.error('[Error]', ...args);
+    console.error("[Error]", ...args);
 }
 
 module.exports = { DEV_MODE, log, logError };
